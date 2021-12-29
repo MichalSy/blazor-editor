e521dd0832a819bf000629f4726a7f29ef9b5588
Michal Sytko <michal.sytko@gmail.com>
2021-12-29 19:42:15 +0100

    Update doc

--- upload.bat
+++
@@ -1,5 +0,0 @@
-del .\build\publish.zip
-powershell Compress-Archive .\build\wwwroot\* .\build\publish.zip
-ssh root@sytko.de rm -r /opt/docker/project/blazor_test/http/*
-scp -r .\build\publish.zip root@sytko.de:/opt/docker/project/blazor_test/http
-ssh root@sytko.de unzip /opt/docker/project/blazor_test/http/publish.zip -d /opt/docker/project/blazor_test/http/
